02014b7524cf77cb38cf38353dc1c9111ad344c3
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2019-12-21 19:39:42 +0000

    adding complete impl of selection sort

--- src/com.sort/SelectionSort.java
+++ src/com.sort/SelectionSort.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -11,13 +12,39 @@ public class SelectionSort {
         this.input = input;
     }
 
-    public ArrayList<Integer> Sort() {
+    private Integer getMin(List<Integer> input) {
+        if (input.size() == 1) {
+            return input.get(0);
+        }
+        int min = input.get((input.size() - 1)); 
+        for (int i=0; i < input.size(); i++) {
+            if (input.get(i) < min) {
+                min = input.get(i);
+            }
+        }
+        return min;
+    }
 
+    public ArrayList<Integer> SortArray() {
+        Integer smallest;
+        ArrayList<Integer> sorted = new ArrayList<Integer>(Arrays.asList());
+        int end = (this.input.size() - 1);
 
+        for (int i=0; i <= end; i++) {
+            smallest = getMin(this.input);
+            sorted.add(smallest);
+            this.input.remove(smallest);
+        }
+        return sorted;
     }
 
 
     public static void main(String []args) {
 
+        ArrayList<Integer> in = 
+        new ArrayList<Integer>(Arrays.asList(30, 4, 7, 11, 8 ,3 ,5 ,1, 300, 78, 3, 5, 9, 450, 6, 79, 22, 333344, 56, 232, 23, 87));
+
+        SelectionSort sort = new SelectionSort(in);
+        System.out.println(sort.SortArray());
     }
 }
